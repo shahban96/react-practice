fa2cc487961e146436b14782f11ff89adf44f0bb
Shahban Ali <shahbanalirajput@gmail.com>
2025-09-01 22:35:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+    <h1>Welcome to React series-01</h1>
+    <Chai />
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Chai.js
@@ -0,0 +1,8 @@
+function Chai()
+{
+    return(
+        <h2>Welcome to React Chai series</h2>
+    )
+}
+
+export default Chai
